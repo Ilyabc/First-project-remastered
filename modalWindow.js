214604f60aea78e4edd708b9ba2685e9362df4e6
Ilyabcc <ilyabcc1@gmail.com>
2022-10-05 18:47:35 +0200

    changes

--- modalWindow.js
+++ modalWindow.js
@@ -3,8 +3,8 @@ const btnPop = document.querySelector('.hero__buttonorder')
 const btnClose = document.querySelector('.button')
 const burger = document.querySelector('.burger')
 const burgerDiv = document.querySelector('.burgerDiv') 
-const burgerClose = document.querySelector(".burger__close")
-
+const burgerClose = document.querySelector('.burger__close')
+const text = document.querySelector('.checkbox__text')
 btnPop.addEventListener('click', function() {
     popup.style.display = 'block';
 })
@@ -20,7 +20,10 @@ btnClose.addEventListener('click', function() {
         document.getElementById('tel').value=''
         document.getElementById('name').value=''
     } else { 
-        alert("Cогласись с правилами конфедициальности")
+        // alert("Cогласись с правилами конфедициальности")
+        text.style.color='red'
+        text.style.transition='500ms cubic-bezier(0.4, 0, 0.2, 1)';
+
     }
 })
 burger.addEventListener('click', function() {
